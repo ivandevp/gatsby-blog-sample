f3fb217cd7ba4f865dd2170453b9e181a8156728
ivandevp <medinadiazivan@gmail.com>
2020-01-06 17:09:19 -0600

    Add dynamic blogpost content

---
+++ gatsby-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Frontend Masters Gatsby Workshop',
+    description: 'A site we built together during a full-day Frontend Masters Gatsby Workshop',
+  },
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js'),
+        },
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: 'posts',
+      },
+    },
+  ],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,22 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout';
+import PostPreview from '../components/post-preview';
+import usePosts from '../hooks/use-posts';
 
-export default () => <div>Hello Frontend Masters!</div>;
+export default () => {
+  const posts = usePosts();
+
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <p>Hello Mexico City!</p>
+      <Link to="/about">Learn about me &rarr;</Link>
+
+      <h2>Read my blog</h2>
+      {posts.map(post => (
+        <PostPreview key={post.slug} post={post} />
+      ))}
+    </Layout>
+  );
+}
